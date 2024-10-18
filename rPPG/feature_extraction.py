df84fb7aa7eb8ab198c0e1a18d7b3f66366d5654
Ren1100 <118090353+Ren1100@users.noreply.github.com>
2024-10-18 10:39:03 +0700

    Delete rPPG directory

--- rPPG/feature_extraction.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-def extract_features_from_frame(frame):
-    green_channel = frame[:, :, 1]
-    avg_intensity = green_channel.mean()
-    var_intensity = green_channel.var()
-    std_intensity = green_channel.std()
-    return [avg_intensity, var_intensity, std_intensity]
