59b2633544181fe331075451a2999f250c7406e6
Ren1100 <118090353+Ren1100@users.noreply.github.com>
2024-11-04 11:32:26 +0700

    Markdown

--- evaluation.py
+++ evaluation.py
@@ -20,6 +20,7 @@ subject_names = ['01-01/', '01-02/', '01-03/', '01-04/', '01-05/', '01-06/',
                  '08-01/', '08-02/', '08-03/', '08-04/', '08-05/', '08-06/',
                  '09-01/', '09-02/', '09-03/', '09-04/', '09-05/', '09-06/',
                  '10-01/', '10-02/', '10-03/', '10-04/', '10-05/', '10-06/',]  # Add more subjects here
+
 dataset_path = 'PURE/'
 
 fps = 30
@@ -30,7 +31,7 @@ top_expand_ratio = 0.25
 detector = dlib.get_frontal_face_detector()  # Face detection
 
 # DataFrame to store BPM results
-bpm_results = pd.DataFrame(columns=['Subject', 'BPM'])
+bpm_results = pd.DataFrame(columns=['Subject', 'POS BPM', 'GRGB BPM'])
 
 for subject_name in subject_names:
     img_frame_path = dataset_path + subject_name + subject_name
@@ -47,7 +48,7 @@ for subject_name in subject_names:
 
     face_left, face_top, face_right, face_bottom = 0, 0, 0, 0
     n_skinpixels = 0
-    mean_rgb = []
+    mean_rgb = np.empty((0, 3))
 
     while i_cnt >= idx_frame_start and i_cnt <= idx_frame_end:
         frame = cv2.imread(png_list[i_cnt])
@@ -72,11 +73,8 @@ for subject_name in subject_names:
 
         face = frame[face_top:face_bottom, face_left:face_right]
 
-        # Convert the face region to hsv color space
-        face_YCrCb = cv2.cvtColor(face, cv2.COLOR_BGR2YCrCb)
-        face_hsv = cv2.cvtColor(face, cv2.COLOR_BGR2HSV)
-
         # Define the skin color range in YCrCb
+        face_YCrCb = cv2.cvtColor(face, cv2.COLOR_BGR2YCrCb)
         lower_skin_YCrCb = np.array([0, 133, 77], dtype=np.uint8)
         upper_skin_YCrCb = np.array([255, 173, 127], dtype=np.uint8)
         mask_YCrCb = cv2.inRange(face_YCrCb, lower_skin_YCrCb, upper_skin_YCrCb)
@@ -84,8 +82,9 @@ for subject_name in subject_names:
         # masked_face = cv2.bitwise_and(face, face, mask=mask_YCrCb)
 
         # Define the skin color range in HSV
-        lower_skin_hsv = np.array([0, 50, 0], dtype=np.uint8)  # Adjust as needed
-        upper_skin_hsv = np.array([25, 150, 255], dtype=np.uint8)  # Adjust as needed
+        face_hsv = cv2.cvtColor(face, cv2.COLOR_BGR2HSV)
+        lower_skin_hsv = np.array([0, 15, 0], dtype=np.uint8)  # Adjust as needed
+        upper_skin_hsv = np.array([17, 170, 255], dtype=np.uint8)  # Adjust as needed
         mask_hsv = cv2.inRange(face_hsv, lower_skin_hsv, upper_skin_hsv)
         # n_skinpixels = np.sum(mask_hsv)
         # masked_face = cv2.bitwise_and(face, face, mask=mask_hsv)
@@ -107,50 +106,52 @@ for subject_name in subject_names:
         f_cnt += 1
         i_cnt += 1
 
+    ##POS##
     # Process the extracted RGB values to calculate rPPG signals
-    # l = int(fps * 1.6)
-    # rPPG_signals = np.zeros(mean_rgb.shape[0])
-    # for t in range(0, mean_rgb.shape[0] - l):
-    #     C = mean_rgb[t:t+l-1, :].T
-    #     mean_color = np.mean(C, axis=1)
-    #     diag_mean_color_inv = np.linalg.inv(np.diag(mean_color))
-    #     Cn = np.matmul(diag_mean_color_inv, C)
-    #     projection_matrix = np.array([[0, 1, -1], [-2, 1, 1]])
-    #     S = np.matmul(projection_matrix, Cn)
-    #     std = np.array([1, np.std(S[0, :]) / np.std(S[1, :])])
-    #     P = np.matmul(std, S)
-    #     epsilon = 1e-8
-    #     rPPG_signals[t:t+l-1] = rPPG_signals[t:t+l-1] + (P - np.mean(P)) / (np.std(P) + epsilon)
+    l = int(fps * 1.6)
+    rPPG_signals = np.zeros(mean_rgb.shape[0])
+    for t in range(0, mean_rgb.shape[0] - l):
+        C = mean_rgb[t:t+l-1, :].T
+        mean_color = np.mean(C, axis=1)
+        diag_mean_color_inv = np.linalg.inv(np.diag(mean_color))
+        Cn = np.matmul(diag_mean_color_inv, C)
+        projection_matrix = np.array([[0, 1, -1], [-2, 1, 1]])
+        S = np.matmul(projection_matrix, Cn)
+        std = np.array([1, np.std(S[0, :]) / np.std(S[1, :])])
+        P = np.matmul(std, S)
+        epsilon = 1e-8
+        rPPG_signals[t:t+l-1] = rPPG_signals[t:t+l-1] + (P - np.mean(P)) / (np.std(P) + epsilon)
+    # Filter the rPPG signals
+    lowcut = 0.65
+    highcut = 4
+    b, a = signal.butter(6, [lowcut, highcut], btype='bandpass', fs=fps)
+    rPPG_filtered = signal.filtfilt(b, a, rPPG_signals)
+
+    # Standardize the filtered signals
+    rPPG_filtered = (rPPG_filtered - np.mean(rPPG_filtered)) / np.std(rPPG_filtered)
+
+    # Calculate the BPM from the rPPG signals
+    seg_len = (2 * rPPG_filtered.shape[0]) // n_segment + 1
+    freq_rPPG, psd_rPPG = signal.welch(rPPG_filtered, fs=fps, nperseg=seg_len, window='flattop')
+    max_freq_rPPG = freq_rPPG[np.argmax(psd_rPPG)]
+    rPPG_bpm = max_freq_rPPG * 60
+    print(f"Subject {subject_name}: BPM: {rPPG_bpm}")
+
+
+    ##GRGB##
     R = mean_rgb[:, 0]
     G = mean_rgb[:, 1]
     B = mean_rgb[:, 2]
 
     GRGB_signal = (G/R) + (G/B)
-
-    # # Filter the rPPG signals
-    # lowcut = 0.65
-    # highcut = 4
-    # b, a = signal.butter(6, [lowcut, highcut], btype='bandpass', fs=fps)
-    # rPPG_filtered = signal.filtfilt(b, a, rPPG_signals)
-
-    # # Standardize the filtered signals
-    # rPPG_filtered = (rPPG_filtered - np.mean(rPPG_filtered)) / np.std(rPPG_filtered)
-
     # Filter the GRGB signals
     lowcut = 0.65
     highcut = 4
     b, a = signal.butter(6, [lowcut, highcut], btype='bandpass', fs=fps)
     GRGB_filtered = signal.filtfilt(b, a, GRGB_signal)
 
-    #standardization
+    # standardization
     GRGB_filtered = (GRGB_filtered-np.mean(GRGB_filtered))/np.std(GRGB_filtered)
-
-    # # Calculate the BPM from the rPPG signals
-    # seg_len = (2 * rPPG_filtered.shape[0]) // n_segment + 1
-    # freq_rPPG, psd_rPPG = signal.welch(rPPG_filtered, fs=fps, nperseg=seg_len, window='flattop')
-    # max_freq_rPPG = freq_rPPG[np.argmax(psd_rPPG)]
-    # rPPG_bpm = max_freq_rPPG * 60
-    # print(f"Subject {subject_name}: BPM: {rPPG_bpm}")
 
     # Calculate the BPM from the rPPG signals
     seg_len = (2 * GRGB_filtered.shape[0]) // n_segment + 1
@@ -159,8 +160,10 @@ for subject_name in subject_names:
     GRGB_bpm = max_freq_GRGB * 60
     print(f"Subject {subject_name}: BPM: {GRGB_bpm}")
 
+    print(f"Subject {subject_name}: Total Frames = {len(png_list)}, RGB = {len(mean_rgb)}")
+
     # Store the BPM result in the DataFrame
-    new_row = pd.DataFrame({'Subject': [subject_name], 'BPM': [GRGB_bpm]})
+    new_row = pd.DataFrame({'Subject': [subject_name], 'POS BPM': [rPPG_bpm], 'GRGB BPM': [GRGB_bpm]})
     bpm_results = pd.concat([bpm_results, new_row], ignore_index=True)
 
 # Save BPM results to an Excel file
